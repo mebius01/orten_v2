f82d2da08a49e674d846465ff0de195f3e5ecd96
iv <consmebius@gmail.com>
2021-04-03 21:11:54 +0300

    add pagination in sitemap & i18n

--- orten_v2/sitemaps.py
+++ orten_v2/sitemaps.py
@@ -1,30 +1,43 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
 from shop.models import Product, Services, Polygraphy
+
 
 class ProductSitemap(Sitemap):
   priority = 1.0
   changefreq = 'daily'
+  i18n = True
+  limit = 1000
+
   def items(self):
     return Product.objects.all()
 
   def location(self, item):
-    return '/product/%s' % (item.slug)
+    return reverse('sitemap_product', args=[item.slug])
+
 
 
 class ServicesSitemap(Sitemap):
   priority = 1.0
   changefreq = 'weekly'
+  i18n = True
+  limit = 1000
+
   def items(self):
     return Services.objects.all()
 
   def location(self, item):
-    return '/service/%s' % (item.slug)
+    return reverse('sitemap_services', args=[item.slug])
 
 class PolygraphySitemap(Sitemap):
   priority = 0.5
   changefreq = 'weekly'
+  i18n = True
+  limit = 1000
   def items(self):
     return Polygraphy.objects.all()
 
   def location(self, item):
-    return '/polygraphy/%s' % (item.slug)
+    return reverse('sitemap_polygraphy', args=[item.slug])
--- orten_v2/urls.py
+++ orten_v2/urls.py
@@ -19,22 +19,30 @@ from django.conf.urls.static import static
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import views
 from .sitemaps import ProductSitemap, PolygraphySitemap, ServicesSitemap
 
-sitemaps = {'product': ProductSitemap, 'polygraphy': PolygraphySitemap, 'services': ServicesSitemap}
+from shop.views import *
+
+sitemaps = {'product': ProductSitemap, 'polygraphy': PolygraphySitemap, 'services': ServicesSitemap }
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin_tools/', include('admin_tools.urls')),
     path('api/', include('order.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('sitemap.xml', views.index, {'sitemaps': sitemaps}),
+    path('sitemap-<section>.xml', views.sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
-    # path('api/', include('order.urls')), # ! fix this
     path('api/', include('shop.urls')),
-    
+
+    # Crutch for sitemap
+    path('service/<slug:slug>/', ServicesDetail.as_view(), name = 'sitemap_services'),
+    path('product/<slug:slug>/', ProductDetail.as_view(), name='sitemap_product'),
+    path('polygraphy/<slug:slug>/', PolygraphyDetail.as_view(), name='sitemap_polygraphy'),
     prefix_default_language=True,) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
